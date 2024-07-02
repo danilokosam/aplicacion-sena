3a90da59d5262ed7368bb97b53962e8e40ce6564
Daniel Duarte <danielduartemora27@gmail.com>
2024-07-02 17:56:41 +0200

    hicimos cambios en los archivos de models.py y views.py, para obtener los datos dinamicamente

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,46 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
-
+from django.core.exceptions import ValidationError
 
 def get_image_filename(instance, filename):
     ext = filename.split(".")[-1]
     filename = f"{instance.documento}.{ext}"
-    return f"usuario/usuarios/{filename}" # Pendiente de cambiar por otro nombre mas concreto
-
-# Create your models here.
-
+    return f"usuario/usuarios/{filename}"
 
 class Usuario(models.Model):
-    primer_nombre = models.CharField(
-        max_length=45, verbose_name="Primer Nombre", blank=True, null=True)
-    segundo_nombre = models.CharField(
-        max_length=45, verbose_name="Segundo Nombre", blank=True, null=True)
-
-    primer_apellido = models.CharField(
-        max_length=45, verbose_name="Primer Apellido", blank=True, null=True)
-    segundo_apellido = models.CharField(
-        max_length=45, verbose_name="Segundo Apellido", blank=True, null=True)
-
+    primer_nombre = models.CharField(max_length=45, verbose_name="Primer Nombre", blank=True, null=True)
+    segundo_nombre = models.CharField(max_length=45, verbose_name="Segundo Nombre", blank=True, null=True)
+    primer_apellido = models.CharField(max_length=45, verbose_name="Primer Apellido", blank=True, null=True)
+    segundo_apellido = models.CharField(max_length=45, verbose_name="Segundo Apellido", blank=True, null=True)
     fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
-    imagen = models.ImageField(upload_to=get_image_filename,
-                               blank=True, null=True, default=r"usuarios\default-user.jpeg")
-    correo = models.EmailField(
-        max_length=50, verbose_name="Correo", default="correo@ejemplo.com")
+    imagen = models.ImageField(upload_to=get_image_filename, blank=True, null=True, default=r"usuarios\default-user.jpeg")
+    correo = models.EmailField(max_length=50, verbose_name="Correo", default="correo@ejemplo.com")
 
     class TipoDocumento(models.TextChoices):
         CEDULA = 'CC', _("Cédula")
         TARJETA = 'TI', _("Tarjeta de Identidad")
         CEDULA_EXTRANJERIA = 'CE', _("Cédula de Extrangería")
-    tipo_documento = models.CharField(
-        max_length=2, choices=TipoDocumento.choices, verbose_name="Tipo de Documento")
-    documento = models.PositiveIntegerField(
-        verbose_name="Documento", unique=True)
+    
+    tipo_documento = models.CharField(max_length=2, choices=TipoDocumento.choices, verbose_name="Tipo de Documento")
+    documento = models.PositiveIntegerField(verbose_name="Documento", unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     estado = models.BooleanField(default=True)
 
     def clean(self):
-        self.primer_nombre = self.primer_nombre.title()
+        if self.primer_nombre:
+            self.primer_nombre = self.primer_nombre.title()
+        if User.objects.filter(username=self.user.username).exists() and self.user.pk is None:
+            raise ValidationError("El nombre de usuario ya existe.")
 
     def __str__(self):
         return f"{self.primer_nombre} {self.primer_apellido}"
@@ -52,17 +43,9 @@ class Usuario(models.Model):
     def full_name(self):
         if self.segundo_nombre:
             return f"{self.primer_nombre} {self.segundo_nombre} {self.primer_apellido} {self.segundo_apellido}"
-        else:
-            return f"{self.primer_nombre} {self.primer_apellido} {self.segundo_apellido}"
+        return f"{self.primer_nombre} {self.primer_apellido} {self.segundo_apellido}"
 
     def usuario_activo(self):
         if self.estado:
             return Usuario.objects.filter(usuario=self, estado=True)
-        else:
-            return Usuario.objects.none()
-
-  # PARA DARLE NOMBRE A LA TABLA
-    # class Meta:
-    #     db_table = "Usuarios"
-    #     verbose_name = _("Usuario")
-    #     verbose_name_plural = _("Usuarios")
+        return Usuario.objects.none()
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render, redirect
-
 from usuarios.forms import UsuarioEditarForm, UsuarioForm
 from usuarios.models import Usuario
-
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.contrib import messages
-
-
 from PIL import Image
-# Create your views here.
-
 
 def usuario_list_view(request, id=None):
     titulo = "Usuarios"
@@ -20,29 +14,27 @@ def usuario_list_view(request, id=None):
         form = UsuarioEditarForm(request.POST, request.FILES, instance=usuario)
         if form.is_valid():
             usuario = form.save()
-            messages.success(
-                request, f'¡El Usuario se editó de forma exitosa!')
+            messages.success(request, '¡El Usuario se editó de forma exitosa!')
             return redirect("usuarios-listar")
         else:
-            form = UsuarioEditarForm(
-                request.POST, request.FILES, instance=usuario)
-            messages.error(request, f'¡Error al editar usuario!')
+            messages.error(request, '¡Error al editar usuario!')
 
     elif request.method == 'POST':
-        form = UsuarioForm(request.POST)
+        form = UsuarioForm(request.POST, request.FILES)
         if form.is_valid():
-            if not User.objects.filter(username=request.POST.get('documento')).exists():
+            documento = request.POST.get('documento')
+            if not User.objects.filter(username=documento).exists():
+                password = make_password("@" + request.POST['primer_nombre'][0] + request.POST['primer_apellido'][0] + request.POST['documento'][-4:])
                 user = User.objects.create_user(
-                    'nombre', 'email@mail', 'password')
-                user.username = request.POST.get('documento')
-                user.first_name = request.POST.get('primer_nombre')
-                user.last_name = request.POST.get('primer_apellido')
-                user.email = request.POST.get('correo')
-                user.password = make_password(
-                    "@" + request.POST['primer_nombre'][0] + request.POST['primer_apellido'][0] + request.POST['documento'][-4:])
-                user.save()
+                    username=documento,
+                    first_name=request.POST.get('primer_nombre'),
+                    last_name=request.POST.get('primer_apellido'),
+                    email=request.POST.get('correo'),
+                    password=password
+                )
             else:
-                user = User.objects.get(username=request.POST.get('documento'))
+                user = User.objects.get(username=documento)
+
             usuario = Usuario.objects.create(
                 primer_nombre=request.POST.get('primer_nombre'),
                 segundo_nombre=request.POST.get('segundo_nombre'),
@@ -52,33 +44,33 @@ def usuario_list_view(request, id=None):
                 imagen=request.FILES.get('imagen'),
                 correo=request.POST.get('correo'),
                 tipo_documento=request.POST.get('tipo_documento'),
-                documento=request.POST.get('documento'),
+                documento=documento,
                 user=user
             )
+
             if usuario.imagen:
                 img = Image.open(usuario.imagen.path)
                 img = img.resize((500, 500))
                 img.save(usuario.imagen.path)
+
             usuario.save()
-            messages.success(
-                request, f'¡El Usuario se agregó de forma exitosa!')
+            messages.success(request, '¡El Usuario se agregó de forma exitosa!')
             return redirect('usuarios-listar')
         else:
-            form = UsuarioForm(request.POST, request.FILES)
-            messages.error(request, f'¡Error al agregar usuario!')
+            messages.error(request, '¡Error al agregar usuario!')
+
     else:
-        if (id):
+        if id:
             usuario = Usuario.objects.get(id=id)
             form = UsuarioEditarForm(instance=usuario)
         else:
             form = UsuarioForm()
+
     context = {
         "titulo": titulo,
         "form": form,
     }
-    # Revisar esta dirección
     return render(request, 'admin/comunidad/usuarios.html', context)
-
 
 def usuario_delete_view(request, id):
     usuario = Usuario.objects.filter(id=id)
